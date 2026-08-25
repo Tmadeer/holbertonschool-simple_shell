a27db343d4a0c1b99c96ccc8a0187b855e1ea2f6
d7ome989 <d.7.o.m.e.989@gmail.com>
2026-08-25 14:41:01 +0300

    Add simple shell implementation and add the libary UNIX

--- main.c
+++ main.c
@@ -1,12 +1,50 @@
-#include <stdio.h>
+#include "main.h"
+extern char **environ;
+
 
 /**
- * main - Entry point
+ * main - Entry point for the simple shell
+ * @ac: Argument count
+ * @av: Argument vector
  *
- * Return: Always 0 (Success)
+ * Return: Always 0 on success
  */
-int main(void)
+int main(int ac, char **av)
 {
-	printf("!\n");
+	char *line = NULL;
+	size_t len = 0;
+	size_t len_line;
+	char *args[2];
+	pid_t child_pid;
+
+	(void)ac;
+
+	while (1)
+	{
+		printf("$ ");
+		if (getline(&line, &len, stdin) == -1)
+			break;
+		len_line = strlen(line);
+		if (len_line > 0 && line[len_line - 1] == '\n')
+			line[len_line - 1] = '\0';
+		if (line[0] == '\0')
+			continue;
+		args[0] = line;
+		args[1] = NULL;
+		child_pid = fork();
+		if (child_pid == 0)
+		{
+			if (execve(args[0], args, environ) == -1)
+			{
+				perror(av[0]);
+				exit(EXIT_FAILURE);
+			}
+		}
+		else
+		{
+			wait(NULL);
+		}
+	}
+	free(line);
 	return (0);
 }
--- main.h
+++ main.h
@@ -1,9 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/* Standard C Libraries for I/O, memory, and strings */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+/* UNIX System Calls for process management */
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 #endif
